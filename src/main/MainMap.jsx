bc2d46d5ab0391cce1a7634d6e51f3916289a04d
wr4eng <wratmoko@gmail.com>
2026-05-19 11:04:18 +0700

    Create MainMap.jsx

---
+++ src/main/MainMap.jsx
@@ -0,0 +1,77 @@
+// src/main/MainMap.jsx
+
+import { useCallback } from 'react';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useDispatch, useSelector } from 'react-redux';
+import MapView from '../map/core/MapView';
+import MapSelectedDevice from '../map/main/MapSelectedDevice';
+import MapAccuracy from '../map/main/MapAccuracy';
+import MapGeofence from '../map/MapGeofence';
+import MapCurrentLocation from '../map/MapCurrentLocation';
+import PoiMap from '../map/main/PoiMap';
+import MapPadding from '../map/MapPadding';
+import { devicesActions } from '../store';
+import MapDefaultCamera from '../map/main/MapDefaultCamera';
+import MapLiveRoutes from '../map/main/MapLiveRoutes';
+import MapLiveRoutesSnap from '../map/main/MapLiveRoutesSnap';
+import MapPositions from '../map/MapPositions';
+import MapOverlay from '../map/overlay/MapOverlay';
+import MapGeocoder from '../map/geocoder/MapGeocoder';
+import MapScale from '../map/MapScale';
+import MapNotification from '../map/notification/MapNotification';
+import useFeatures from '../common/util/useFeatures';
+
+const MainMap = ({ filteredPositions, selectedPosition, onEventsClick }) => {
+  const theme = useTheme();
+  const dispatch = useDispatch();
+
+  const desktop = useMediaQuery(theme.breakpoints.up('md'));
+
+  const eventsAvailable = useSelector((state) => !!state.events.items.length);
+
+  const features = useFeatures();
+
+  const onMarkerClick = useCallback(
+    (_, deviceId) => {
+      dispatch(devicesActions.selectId(deviceId));
+    },
+    [dispatch],
+  );
+
+  return (
+    <>
+      <MapView>
+        <MapOverlay />
+        <MapGeofence />
+        <MapAccuracy positions={filteredPositions} />
+        <MapLiveRoutes deviceIds={filteredPositions.map((p) => p.deviceId)} />
+        <MapLiveRoutesSnap deviceIds={filteredPositions.map((p) => p.deviceId)} />
+        <MapPositions
+          positions={filteredPositions}
+          onMarkerClick={onMarkerClick}
+          selectedPosition={selectedPosition}
+          showStatus
+        />
+        <MapDefaultCamera />
+        <MapSelectedDevice />
+        <PoiMap />
+      </MapView>
+      <MapScale />
+      <MapCurrentLocation />
+      <MapGeocoder />
+      {!features.disableEvents && (
+        <MapNotification enabled={eventsAvailable} onClick={onEventsClick} />
+      )}
+      {desktop && (
+        <MapPadding
+          start={
+            parseInt(theme.dimensions.drawerWidthDesktop, 10) + parseInt(theme.spacing(1.5), 10)
+          }
+        />
+      )}
+    </>
+  );
+};
+
+export default MainMap;
